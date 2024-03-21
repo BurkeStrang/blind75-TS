3c4417f95c67c78c2a123a2f183a995e0fd88df5
Burke.Strang <burke.strang@vertigis.com>
2024-03-21 10:03:42 -0400

    new pointers

---
+++ test/TwoPointers/threesum.test.ts
@@ -0,0 +1,7 @@
+import { threeSum } from "../../TwoPointers/threesum";
+
+describe("ThreeSum", () => {
+  test("Example test case 1", () => {
+    expect(threeSum([-1, 0, 1, 2, -1, -4])).toStrictEqual([[-1, -1, 2], [-1, 0, 1]])
+  })
+});
