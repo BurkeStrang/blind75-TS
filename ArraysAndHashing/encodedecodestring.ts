a8517912c8b9dd39546527f09ef2731516999640
Burke.Strang <burke.strang@vertigis.com>
2024-03-21 09:28:58 -0400

    finishing arrays

---
+++ ArraysAndHashing/encodedecodestring.ts
@@ -0,0 +1,18 @@
+function encodeString(s: string): string {
+  let result = '';
+  for (let i = 0; i < s.length; i++) {
+    result += s.charCodeAt(i) + '.';
+  }
+  return result;
+}
+
+function decodeString(s: string): string {
+  let result = '';
+  const arr = s.split('.');
+  for (let i = 0; i < arr.length - 1; i++) {
+    result += String.fromCharCode(Number(arr[i]));
+  }
+  return result;
+}
+
+export { encodeString, decodeString };
---
+++ test/ArraysAndHashing/productofArrayExceptSelf.test.ts
@@ -0,0 +1,7 @@
+import { productofArrayExceptSelf } from "../../ArraysAndHashing/productofArrayExceptSelf";
+
+describe('productofArrayExceptSelf', () => {
+  test('Example 1', () => {
+    expect(productofArrayExceptSelf([1, 2, 3, 4])).toStrictEqual([24, 12, 8, 6])
+  })
+});
