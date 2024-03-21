1b420624a2f8d2556077925dbf074d9912472000
Burke.Strang <burke.strang@vertigis.com>
2024-03-21 10:12:13 -0400

    add new test

---
+++ test/SlidingWindow/besttimetobuyandsellstock.test.ts
@@ -0,0 +1,7 @@
+import { maxProfit } from "../../SlidingWindow/besttimetobuyandsellstock";
+
+describe("maxProfit", () => {
+  test("Example test case 1", () => {
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toBe(5)
+  })
+});
