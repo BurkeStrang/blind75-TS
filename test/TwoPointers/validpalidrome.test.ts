994bc4ce0d935e516b08616684f8ce90657905dd
Burke.Strang <burke.strang@vertigis.com>
2024-03-21 09:57:59 -0400

    twopointers

---
+++ test/TwoPointers/validpalidrome.test.ts
@@ -0,0 +1,7 @@
+import { isPalindrome } from "../../TwoPointers/validpalidrome";
+
+describe('Valid Palindrome', () => {
+  test('Example 1', () => {
+    expect(isPalindrome("race a car")).toStrictEqual(false)
+  })
+});
