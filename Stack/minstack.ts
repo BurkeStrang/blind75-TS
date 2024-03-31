7108262bf9c87ff00e43c794491a45475894fd56
Burke.Strang <burke.strang@vertigis.com>
2024-03-31 09:22:41 -0400

    adding more stacks

--- Stack/minstack.ts
+++ Stack/minstack.ts
@@ -63,3 +63,5 @@ class MinStack {
   }
 }
 
+export { MinStack };
+
---
+++ test/Stack/reversepolish.test.ts
@@ -0,0 +1,7 @@
+import { evalRPN } from '../../Stack/reversepolish'
+
+describe('evalRPN', () => {
+  test('evalRPN', () => {
+    expect(evalRPN(["2", "1", "+", "3", "*"])).toStrictEqual(9)
+  })
+});
