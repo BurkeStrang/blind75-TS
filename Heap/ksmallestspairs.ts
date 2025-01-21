99be8de1b22dcde38bcdc20f1238b10c8ff7376f
Burke Strang <Burke.Strang@vertigis.com>
2025-01-20 20:29:32 -0500

    wip

--- Heap/ksmallestspairs.ts
+++ Heap/ksmallestspairs.ts
@@ -1,5 +1,17 @@
 import MinHeap from "../Shared/min_heap";
 
+/**
+ * Returns the k smallest pairs in number array of array.
+ *
+ * @param name list1 the first list.
+ * @param name list2 the second list.
+ * @returns returns the k smallest pairs in number array of array.
+ * @example
+ * ```
+ * Input: list1 = [1,7,11], list2 = [2,4,6], k = 3
+ * Ouput: [[1,2],[1,4],[1,6]]
+ * ```
+ */
 export function kSmallestPairs(list1: number[], list2: number[], k: number) : number[][] {
   const listLength = list1.length;
   const minHeapForPairs = new MinHeap<[number, number, number]>();
@@ -8,6 +20,7 @@ export function kSmallestPairs(list1: number[], list2: number[], k: number) : nu
   for (let i = 0; i < Math.min(k, listLength); i++) {
     minHeapForPairs.offer([list1[i] + list2[0], i, 0]);
   }
+  console.log(minHeapForPairs);
 
   let counter = 1;
 
