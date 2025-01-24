4fb9de71af8e7cc9bec0b08a4c7171a485ef5ab7
Burke Strang <Burke.Strang@vertigis.com>
2025-01-24 12:20:42 -0500

    wip

--- Heap/ksmallestspairs.ts
+++ Heap/ksmallestspairs.ts
@@ -12,27 +12,35 @@ import MinHeap from "../Shared/min_heap";
  * Ouput: [[1,2],[1,4],[1,6]]
  * ```
  */
-export function kSmallestPairs(list1: number[], list2: number[], k: number) : number[][] {
-  const listLength = list1.length;
-  const minHeapForPairs = new MinHeap<[number, number, number]>();
-  const pairs = [];
+export function kSmallestPairs(
+    list1: number[],
+    list2: number[],
+    k: number
+): number[][] {
+    const listLength = list1.length;
+    const minHeapForPairs = new MinHeap<[number, number, number]>();
+    const pairs = [];
 
-  for (let i = 0; i < Math.min(k, listLength); i++) {
-    minHeapForPairs.offer([list1[i] + list2[0], i, 0]);
-  }
-  console.log(minHeapForPairs);
-
-  let counter = 1;
-
-  while (minHeapForPairs.size() > 0 && counter <= k) {
-    const [, i, j] = minHeapForPairs.poll()!;
-    pairs.push([list1[i], list2[j]]);
-    const nextElement = j + 1;
-
-    if (list2.length > nextElement) {
-      minHeapForPairs.offer([list1[i] + list2[nextElement], i, nextElement]);
+    for (let i = 0; i < Math.min(k, listLength); i++) {
+        minHeapForPairs.offer([list1[i] + list2[0], i, 0]);
     }
-    counter++;
-  }
-  return pairs;
+    console.log(minHeapForPairs);
+
+    let counter = 1;
+
+    while (minHeapForPairs.size() > 0 && counter <= k) {
+        const [, i, j] = minHeapForPairs.poll()!;
+        pairs.push([list1[i], list2[j]]);
+        const nextElement = j + 1;
+
+        if (list2.length > nextElement) {
+            minHeapForPairs.offer([
+                list1[i] + list2[nextElement],
+                i,
+                nextElement,
+            ]);
+        }
+        counter++;
+    }
+    return pairs;
 }
---
+++ test/Heap/kthsmallestnumber.test.ts
@@ -0,0 +1,33 @@
+
+import { kthSmallestNumber} from '../../Heap/kthsmallestnumber';
+
+describe('Task Schedueler', () => {
+  test('Testcase 1', () => {
+    const matrix = [
+      [1, 5, 9],
+      [10, 11, 13],
+      [12, 13, 15]
+    ]
+    const k = 8;
+    expect(kthSmallestNumber(matrix, k)).toEqual(13);
+  })
+  test('Testcase 2', () => {
+    const matrix = [
+      [1, 5, 9],
+      [10, 11, 13],
+      [12, 13, 15]
+    ]
+    const k = 4;
+    expect(kthSmallestNumber(matrix, k)).toEqual(10);
+  })
+  test('Testcase 3', () => {
+    const matrix = [
+      [1, 5, 9],
+      [10, 11, 13],
+      [12, 13, 15]
+    ]
+    const k = 1;
+    expect(kthSmallestNumber(matrix, k)).toEqual(1);
+  })
+});
+
